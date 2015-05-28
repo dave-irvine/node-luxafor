9a34bfafeb10f204f2d6cf6b46bb9a7b1806f310
Dave Irvine <dave@daveirvine.me>
2015-05-28 12:19:58 +0100

    Added flashColor() and setColor()

--- index.js
+++ index.js
@@ -59,6 +59,52 @@ Luxafor.prototype.setLuxaforColor = function (color, cb) {
 	});
 };
 
+Luxafor.prototype.flashColor = function (r, g, b, cb) {
+	var buff = new Buffer(8);
+
+	//Strobe
+	buff.writeUInt8(3, 0);
+	//"Both Sides"
+	buff.writeUInt8(255, 1);
+
+	buff.writeUInt8(r, 2);
+	buff.writeUInt8(g, 3);
+	buff.writeUInt8(b, 4);
+
+	//"t" 10. Time?
+	buff.writeUInt8(10, 5);
+
+	//"d" ?
+	buff.writeUInt8(0, 6);
+
+	//"Re" 3. Repeat?
+	buff.writeUInt8(3, 7);
+
+	this.endpoint.transfer(buff, function () {
+		if (cb) {
+			cb();
+		}
+	});
+};
+
+Luxafor.prototype.setColor = function  (r, g, b, cb) {
+	var buff = new Buffer(5);
+
+	//Jump
+	buff.writeUInt8(1, 0);
+	//"Both Sides"
+	buff.writeUInt8(255, 1);
+
+	buff.writeUInt8(r, 2);
+	buff.writeUInt8(g, 3);
+	buff.writeUInt8(b, 4);
+
+	this.endpoint.transfer(buff, function () {
+		if (cb) {
+			cb();
+		}
+	});
+};
 
 module.exports = function () {
 	return new Luxafor();
